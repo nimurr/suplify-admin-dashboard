02fca41c38c4046b5b467c90f629f9fb3139c0d7
nimurr <nimurnerob404@gmail.com>
2025-10-13 16:33:19 +0600

    `Fixed import order and removed unnecessary whitespace in Route.jsx`

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -7,20 +7,20 @@ import Main from "../layout/Main";
 import DashboardHome from "../dashboard/home/DashboardHome";
 import Notification from "../dashboard/menu/headermenu/Notification";
 import Profile from "../dashboard/menu/headermenu/Profile";
-import EditProfiel from "../dashboard/menu/headermenu/EditProfile"; 
- 
-import PrivacyPolicy from "../dashboard/menu/privacypolicy/PrivacyPolicy";
+import EditProfiel from "../dashboard/menu/headermenu/EditProfile";
+
 import EditPrivacy from "../dashboard/menu/privacypolicy/EditPrivacy";
+import PrivacyPolicy from "../dashboard/menu/privacypolicy/PrivacyPolicy";
 import TermCondition from "../dashboard/menu/termsCondition/TermCondition";
 import EditTermCondition from "../dashboard/menu/termsCondition/EditTermCondition";
 
 import ErrorPage from "./ErrorPage";
 import About from "../dashboard/menu/about/About";
 import EditAbout from "../dashboard/menu/about/EditAbout";
- 
+
 import User from "../dashboard/menu/sidebarMenu/User";
 import Earning from "../dashboard/menu/sidebarMenu/Earning";
- 
+
 import Settings from "../dashboard/menu/sidebarMenu/Settings";
 import Store from "../dashboard/menu/sidebarMenu/Store";
 import ViewProfile from "../dashboard/menu/sidebarMenu/ViewProfile";
@@ -80,41 +80,41 @@ export const router = createBrowserRouter([
                 element: <SupplementCreateForm />
             },
             {
-                path:"user",
+                path: "user",
                 element: <User />
             },
             {
-                path:"user/view-user",
+                path: "user/view-user",
                 element: <ViewProfile />
             },
-            
+
             {
                 path: 'settings',
-                element:<Settings />
+                element: <Settings />
             },
             {
-                path:'settings/privacypolicy',
-                element:<PrivacyPolicy />
+                path: 'settings/privacypolicy',
+                element: <PrivacyPolicy />
             },
             {
-                path:'settings/editprivacypolicy',
+                path: 'settings/editprivacypolicy',
                 element: <EditPrivacy />
             },
             {
-                path:"settings/termcondition",
-                element:<TermCondition />
+                path: "settings/termcondition",
+                element: <TermCondition />
             },
             {
                 path: "settings/edittermcondition",
                 element: <EditTermCondition />
             },
             {
-                path:'settings/about',
+                path: 'settings/about',
                 element: <About />
             },
             {
-                path:'settings/editabout',
-                element:<EditAbout />
+                path: 'settings/editabout',
+                element: <EditAbout />
             },
         ]
     }
