89160e5fe45814a3d8f9cabb8ab275d769c59f57
nimurr <nimurnerob404@gmail.com>
2025-10-19 09:28:14 +0600

    `Refactored Settings page links with updated styles and navigation`

--- src/dashboard/menu/sidebarMenu/Settings.jsx
+++ src/dashboard/menu/sidebarMenu/Settings.jsx
@@ -22,21 +22,21 @@ const Settings = () => {
       <h1 className="font-semibold text-[25px]">Settings</h1>
       <div> 
         {/* Privacy policy */}
-        <div onClick={() => navigate("/dashboard/settings/privacypolicy")} className="mt-8 cursor-pointer flex justify-between bg-[#F7F7F7] rounded items-center w-full h-[75px]">
+        <div onClick={() => navigate("/dashboard/settings/privacypolicy")} className="mt-5 bg-gradient-to-br from-[#8400ff8e] to-[#ff09099f] text-primaryBg cursor-pointer flex justify-between bg-[#F7F7F7] rounded items-center w-full h-[75px]">
           <p className="text-[16px] ml-8 font-medium">Privacy Policy</p>
-          <Link to="/dashboard/settings/privacypolicy" className="mr-8 text-[#193664] px-2 py-1 rounded cursor-pointer">
+          <Link to="/dashboard/settings/privacypolicy" className="mr-8 text-[#f5f5f5] px-2 py-1 rounded cursor-pointer">
             <IoIosArrowForward />
           </Link>
         </div>
-        <div onClick={() => navigate("/dashboard/settings/termcondition")} className="mt-8 flex justify-between cursor-pointer bg-[#F7F7F7] rounded items-center w-full h-[75px]">
+        <div onClick={() => navigate("/dashboard/settings/termcondition")} className="mt-5 bg-gradient-to-br from-[#8400ff8e] to-[#ff09099f] text-primaryBg flex justify-between cursor-pointer bg-[#F7F7F7] rounded items-center w-full h-[75px]">
           <p className="text-[16px] ml-8 font-medium">Terms and Condition</p>
-          <Link to="/dashboard/settings/termcondition" className="mr-8 text-[#193664] px-2 py-1 rounded cursor-pointer">
+          <Link to="/dashboard/settings/termcondition" className="mr-8 text-[#f5f5f5] px-2 py-1 rounded cursor-pointer">
             <IoIosArrowForward />
           </Link>
         </div>
-        <div onClick={() => navigate("/dashboard/settings/about")} className="mt-8 cursor-pointer flex justify-between bg-[#F7F7F7] rounded items-center w-full h-[75px]">
+        <div onClick={() => navigate("/dashboard/settings/about")} className="mt-5 bg-gradient-to-br from-[#8400ff8e] to-[#ff09099f] text-primaryBg cursor-pointer flex justify-between bg-[#F7F7F7] rounded items-center w-full h-[75px]">
           <p className="text-[16px] ml-8 font-medium">About Us</p>
-          <Link to="/dashboard/settings/about" className="mr-8 text-[#193664] px-2 py-1 rounded cursor-pointer">
+          <Link to="/dashboard/settings/about" className="mr-8 text-[#f5f5f5] px-2 py-1 rounded cursor-pointer">
             <IoIosArrowForward />
           </Link>
         </div>
